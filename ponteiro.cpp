29497edae501845834709bb6ef8f3e7a03e16a54
Samuel Fernandes Monteiro <126695495+samuel-fm-coding@users.noreply.github.com>
2024-04-05 21:22:09 -0300

    Create ponteiro.cpp

---
+++ ponteiro.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+
+main(){
+	setlocale(LC_ALL, "Portuguese");
+	
+	int *ponteiro;
+	
+	ponteiro = (int*)malloc(sizeof(int)); //alocando memória
+	
+	printf("Digite um número inteiro: "); //usuario digita um valor pra armazenar na memória
+	scanf("%d", ponteiro);
+	
+	printf("\n\nValor %d armazenado no endereço %d", *ponteiro, ponteiro); //exibo valor(*) e endereço		
+}
